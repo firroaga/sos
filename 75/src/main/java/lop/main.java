3f5fbeb2ad92963e7a83f112915e99459c2ff0ea
Firat Demirci <firro_aga@hotmail.com>
2014-12-09 11:55:47 +0100

    12uur

--- 75/src/main/java/lop/main.java
+++ 75/src/main/java/lop/main.java
@@ -2,19 +2,11 @@ package lop;
 
 public class main {
 
-	public static void main(String[] args) {
+	public int testpoging(int a, int b){
 		// TODO Auto-generated method stub
-		int x = 40;
-
-	      if( x == 10 ){
-	         System.out.print("Value of X is 10");
-	      }else if( x == 20 ){
-	         System.out.print("Value of X is 20");
-	      }else if( x == 30 ){
-	         System.out.print("Value of X is 30");
-	      }else{
-	         System.out.print("END");
-	      }
+		return a * b;
+		
+		
 	   }
 	
 }
